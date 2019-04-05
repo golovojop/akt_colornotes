c9cda457d68e8a59435ae4f579e45a5beb046f08
yagray <yagray@mail.ru>
2019-04-05 15:34:04 +0300

    ok

--- app/src/main/java/yarlykov/s/colornotes/ui/main/MainViewModel.kt
+++ app/src/main/java/yarlykov/s/colornotes/ui/main/MainViewModel.kt
@@ -5,6 +5,7 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModel
 import yarlykov.s.colornotes.data.entity.Note
+import yarlykov.s.colornotes.data.model.NoteResult
 import yarlykov.s.colornotes.data.model.Repository
 
 class MainViewModel: ViewModel() {
@@ -14,17 +15,19 @@ class MainViewModel: ViewModel() {
     /**
      * TODO: Поставить Observer на изменения в LiveData репозитория. Если в репозитории
      * TODO: что-то меняется, то в локальной LiveData это нужно отразить, тогда
-     * TODO: и MainActivity узнает об изменениях. Получиласть цепочка из LiveData,
-     * TODO: связанная обзерверами.
+     * TODO: и MainActivity узнает об изменениях. Получилась цепочка из LiveData,
+     * TODO: связанная обзерверами. Данный класс не знает где фактически хранятся данные.
      */
     init {
-        Repository.getNotes().observeForever {
-            viewStateLiveData.value = viewStateLiveData.value?.copy(notes = it!!) ?: MainViewState(it!!)
+        Repository.getNotes().observeForever {noteResult ->
+            noteResult?.also{
+                when(it) {
+                    is NoteResult.Success<*> -> viewStateLiveData.value = MainViewState(notes = it.data as List<Note>)
+                    is NoteResult.Error -> it.error
+                }
+            }
         }
     }
 
     fun viewState() : LiveData<MainViewState> = viewStateLiveData
-
-
-
 }
