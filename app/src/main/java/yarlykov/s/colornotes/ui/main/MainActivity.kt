5d7b6e4ff04688a07b8736d394ea33df14456194
yagray <yagray@mail.ru>
2019-04-11 12:32:47 +0300

    В эмуляторе гугловая аутентификация отрабатывает. Появляется окно входа, все ОК. На моем телефоне не стартует из-за того, что я там поотключал Google Play

--- app/src/main/java/yarlykov/s/colornotes/ui/main/MainActivity.kt
+++ app/src/main/java/yarlykov/s/colornotes/ui/main/MainActivity.kt
@@ -1,19 +1,35 @@
 package yarlykov.s.colornotes.ui.main
 
+import android.app.Activity
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat.startActivityForResult
 import android.support.v7.widget.GridLayoutManager
+import android.util.Log
+import com.firebase.ui.auth.AuthUI
 import kotlinx.android.synthetic.main.activity_main.*
 import yarlykov.s.colornotes.R
 import yarlykov.s.colornotes.data.entity.Note
+import yarlykov.s.colornotes.exception.NoAuthException
 import yarlykov.s.colornotes.ui.note.NoteActivity
 
 class MainActivity : AppCompatActivity() {
 
+    val RC_SIGN_IN = 458;
+
     lateinit var viewModel: MainViewModel
     lateinit var adapter: MainAdapter
+
+    companion object {
+        fun start(context: Context) {
+            val intent = Intent(context, MainActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +41,12 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         adapter = MainAdapter(::openNoteScreen)
 
-
         // TODO: В этом месте адаптер инициализируется данными
-        viewModel.viewState().observe(this, Observer<MainViewState> { it ->
-            it?.let { adapter.notes = it.notes }
+        viewModel.viewState().observe(this, Observer<MainViewState> { viewState ->
+            viewState?.apply {
+                notes?.let {renderData(it)}
+                error?.let {renderError(it)}
+            }
         })
 
         // TODO: Котлин позволяет обойтись без findViewById<>() и напрямую обратиться к View
@@ -36,7 +54,45 @@ class MainActivity : AppCompatActivity() {
         rv_noteList.layoutManager = GridLayoutManager(this, 2)
 
         // TODO: При нажатии на кнопку запускаем активити созданиязаметки
-        fab.setOnClickListener{openNoteScreen(null)}
+        fab.setOnClickListener { openNoteScreen(null) }
+    }
+
+    fun renderData(data: List<Note>?) {
+        data?.let {
+            adapter.notes = it
+        }
+    }
+
+    fun renderError(error: Throwable?) {
+        when (error) {
+            is NoAuthException -> startLoginActivity()
+            else -> error?.let {
+                Log.d("NOTES_ALL", it.message)
+            }
+        }
+    }
+
+
+    fun startLoginActivity() {
+        val providers = listOf(
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setLogo(R.drawable.android_robot)
+                .setTheme(R.style.LoginStyle)
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN && resultCode != Activity.RESULT_OK){
+            finish()
+        }
     }
 
     private fun openNoteScreen(note: Note?) {
--- app/src/main/java/yarlykov/s/colornotes/ui/main/MainViewState.kt
+++ app/src/main/java/yarlykov/s/colornotes/ui/main/MainViewState.kt
@@ -2,4 +2,4 @@ package yarlykov.s.colornotes.ui.main
 
 import yarlykov.s.colornotes.data.entity.Note
 
-data class MainViewState (val notes : List<Note>)
+class MainViewState(val notes: List<Note>? = null, val error: Throwable? = null)
